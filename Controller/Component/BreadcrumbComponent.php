26638e640266dd276855d6cb37106a4220143cf0
Andrea Dal Ponte <info@andreadalponte.com>
2013-08-31 16:49:57 +0200

    fx initialization

--- Controller/Component/BreadcrumbComponent.php
+++ Controller/Component/BreadcrumbComponent.php
@@ -2,31 +2,27 @@
 
 class BreadcrumbComponent extends Component {
 
-	protected $_defaultOptions = array(
-		'scope' => 'default'
-	);
+  protected $_defaultOptions = array(
+    'scope' => 'default'
+  );
 
-	protected $_breadCrumbs = array(
-		'default' => array()
-	);
+  protected $_breadCrumbs = array(
+    'default' => array()
+  );
 
-	public function initialize(&$controller, $settings = array()) {
-		$this->_Controller = $controller;
-	}
-	
-	public function add($name = null, $url = null, $options = array()) {
-		$options = array_merge($this->_defaultOptions, $options);
+  public function initialize(Controller $controller) {
+    $this->_Controller = $controller;
+  }
 
-		if(!$this->_breadCrumbs[$options['scope']]) {
-			$this->_breadCrumbs[$options['scope']] = array();
-		}
+  public function add($name = null, $url = null, $options = array()) {
+    $options = array_merge($this->_defaultOptions, $options);
 
-		$this->_breadCrumbs[$options['scope']][$name] = $url;
-		$this->_Controller->set('breadcrumbs', $this->_breadCrumbs);
-	}
-	
-	// public function beforeRender($controller) {
-	// 	$controller->set('breadcrumbs', $this->_breadCrumbs);
-	// }
-	
+    if(!$this->_breadCrumbs[$options['scope']]) {
+      $this->_breadCrumbs[$options['scope']] = array();
+    }
+
+    $this->_breadCrumbs[$options['scope']][$name] = $url;
+    $this->_Controller->set('breadcrumbs', $this->_breadCrumbs);
+  }
+
 }
